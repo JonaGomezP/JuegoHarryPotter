28702fba096ec54ec4f22dacf5bd4879ed50c05e
JonaGomezP <jpg0007@gmail.com>
2022-01-17 22:58:03 +0100

    juego actualizado

--- script.js
+++ script.js
@@ -1,38 +1,81 @@
+
 window.onload = () => {
-    // Posición inicial prota
+
+    //Creo la opacidad de fondo al recargar la página donde se muestran los botones de "jugar" y las dificultades
+    let padre = document.querySelector("body");
+    var opacidadFondo = document.createElement("div");
+    opacidadFondo.style.zIndex = "1";
+    opacidadFondo.style.backgroundColor = "black";
+    opacidadFondo.style.opacity = "1";
+    opacidadFondo.style.width = padre.offsetWidth + "px";
+    opacidadFondo.style.width = "100%";
+    opacidadFondo.style.minHeight = screen.availHeight + "px";
+    opacidadFondo.style.position = "fixed";
+    opacidadFondo.style.top = "0";
+    opacidadFondo.style.left = "0";
+    opacidadFondo.style.display = "flex";
+    opacidadFondo.style.justifyContent = "center";
+    opacidadFondo.style.alignItems = "center";
+    opacidadFondo.style.flexDirection = "column";
+    opacidadFondo.style.margin = "0";
+    //Agrego al body el la opacidad de fondo antes del primer hijo que tenga
+    padre.insertBefore(opacidadFondo, padre.firstChild);
+
+    //Creamos el botón que inicia el juego y le aplicamos estilos
+    let boton = document.createElement("input");
+    boton.onclick = function () {
+        padre.removeChild(opacidadFondo)
+    }
+
+    boton.value = "!JUGAR¡";
+    boton.type = "image";
+    boton.src = "/img/harry.png"
+    boton.position = "absolute";
+    boton.style.fontSize = "66px";
+    boton.style.boxShadow = "5px 10px";
+    boton.style.width = "30%";
+
+    boton.zIndex = "2";
+    boton.onmouseover = function () {
+        boton.style["-webkit-transition"] = "-webkit-transform 500ms linear";
+        boton.style["-webkit-transform"] = "scale(1.2)";
+        boton.style["transform"] = "transform:scale(1.2)";
+    }
+    boton.onmouseout = function () {
+        boton.style["-webkit-transition"] = "-webkit-transform 500ms linear";
+        boton.style["-webkit-transform"] = "scale(1)";
+        boton.style["transform"] = "transform:scale(1)";
+    }
+
+
+    opacidadFondo.appendChild(boton);
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+    // Selecciono todas las celdas de la tabla
+    let celdas = document.querySelectorAll("td");
+
+    // Coloco al protagonista y guardo su posición en una variable
     let posicion_prota = 0;
+    celdas[posicion_prota].id = "prota";
 
-    // Genero la posición de los examenes y los coloco en el tablero
+
+    // Genero la posición de los examenes, guardo dicha posición en una variable y coloco los examenes en el tablero
     let posicion_examenes = parseInt(Math.random() * 62 + 1);
-
-    let examenes = document.querySelectorAll("td")[posicion_examenes];
-    examenes.id = "examenes";
+    celdas[posicion_examenes].id = "examenes";
 
     //Posición inicial malo
     let posicion_malo = parseInt(Math.random() * (63 - 31) + 31);
-    if(posicion_malo == posicion_examenes){
-        posicion_malo = posicion_malo ++;
+    if (posicion_malo == posicion_examenes) {
+        posicion_malo = posicion_malo++;
     }
     let malo = document.querySelectorAll("td")[posicion_malo];
     malo.id = "malo";
-
-    // Selecciono la celda en la que se encuentra el prota
-    let prota = document.querySelectorAll("td")[posicion_prota];
-    
-
-    // Asigno a la celda en la que se encuentra el prota el id "prota"
-    prota.id = "prota";
-
-    // Selecciono el formulario y los botones que hay en su interior
-    let form = document.querySelector("form");
-    let arriba = form.arriba;
-    let izquierda = form.izquierda;
-    let derecha = form.derecha;
-    let abajo = form.abajo;
-
-    // Deshabilito los botones de arriba e izquierda
-    arriba.disabled = true;
-    izquierda.disabled = true;
 
     // Creo una variable inicializada a "false" para almacenar los examenes (false: no tiene los examenes  --  True: tiene los examenes)
     let examanes_inventario = false;
@@ -40,101 +83,105 @@ window.onload = () => {
     // Selecciono el "inventario"
     let inventario = document.getElementById("inventario");
 
-    // Mover hacia abajo
-    abajo.onclick = () => {
-        if (arriba.disabled) {
-            arriba.disabled = false;
-        }
-        prota.id = "";
-        posicion_prota += 8;
-        prota = document.querySelectorAll("td")[posicion_prota];
-        prota.id = "prota";
-        if (posicion_prota >= 56 && posicion_prota <= 63) {
-            abajo.disabled = true;
-        }
-        if (posicion_prota == posicion_examenes) {
-            examanes_inventario = true;
-            inventario.textContent = "Examenes";
-        }
+    // Recorro todas las celdas de la tabla
+    celdas.forEach((i) => {
+        // Añado el evento "onclick" a cada una de las celdas
+        i.onclick = (e) => {
+            // Compruebo si el movimiento que quiere hacer el usuario es correcto
+            if (posicion_prota + 1 == Array.from(celdas).indexOf(e.target) || posicion_prota - 1 == Array.from(celdas).indexOf(e.target) || posicion_prota + 8 == Array.from(celdas).indexOf(e.target) || posicion_prota - 8 == Array.from(celdas).indexOf(e.target)) {
+                celdas[posicion_prota].id = "";
+                posicion_prota = Array.from(celdas).indexOf(e.target);
+                e.target.id = "prota";
+                // Compruebo si el protagonista ha conseguido los examenes
+                if (posicion_prota == posicion_examenes) {
+                    examanes_inventario = true;
+                    inventario.textContent = "Examenes";
+                }
 
-        //Cuando se mueve al prota, se mueve automáticamente al malo
-        let timer = setTimeout(moverMalo,1000);
-    }
+                //Cuando se mueve al prota, se mueve automáticamente al malo
+                let timer = setTimeout(moverMalo, 1000, celdas, Array.from(celdas).indexOf(e.target));
+            }
+            else if (posicion_prota == Array.from(celdas).indexOf(e.target)) {
+                alert("El protagonista no se puede quedar en la misma posición. Intente moverlo a otra posición diferente a la que se encuentra.");
+            }
+            else {
+                alert("Ha esa posición no se puede mover. Intentelo de nuevo con otra posición.");
+            }
 
-    // Mover hacia arriba
-    arriba.onclick = () => {
-        if (abajo.disabled) {
-            abajo.disabled = false;
         }
-        prota.id = "";
-        posicion_prota -= 8;
-        prota = document.querySelectorAll("td")[posicion_prota];
-        prota.id = "prota";
-        if (posicion_prota >= 0 && posicion_prota <= 7) {
-            arriba.disabled = true;
-        }
-        if (posicion_prota == posicion_examenes) {
-            examanes_inventario = true;
-            inventario.textContent = "Examenes";
-        }
+    });
 
-        //Cuando se mueve al prota, se mueve automáticamente al malo
-        let timer = setTimeout(moverMalo,1000);
-    }
-
-    // Mover hacia la izquierda
-    izquierda.onclick = () => {
-        if (derecha.disabled) {
-            derecha.disabled = false;
-        }
-        prota.id = "";
-        posicion_prota -= 1;
-        prota = document.querySelectorAll("td")[posicion_prota];
-        prota.id = "prota";
-        if (posicion_prota % 8 == 0 || posicion_prota == 0) {
-            izquierda.disabled = true;
-        }
-        if (posicion_prota == posicion_examenes) {
-            examanes_inventario = true;
-            inventario.textContent = "Examenes";
-        }
-
-        //Cuando se mueve al prota, se mueve automáticamente al malo
-        let timer = setTimeout(moverMalo,1000);        
-    }
-
-    // Mover hacia la derecha
-    derecha.onclick = () => {
-        if (izquierda.disabled) {
-            izquierda.disabled = false;
-        }
-        const fin_derecha = [7, 15, 23, 31, 39, 47, 55, 63];
-        prota.id = "";
-        posicion_prota += 1;
-        prota = document.querySelectorAll("td")[posicion_prota];
-        prota.id = "prota";
-        if (fin_derecha.includes(posicion_prota)) {
-            derecha.disabled = true;
-        }
-        if (posicion_prota == posicion_examenes) {
-            examanes_inventario = true;
-            inventario.textContent = "Examenes";
-        }
-
-        
-        //Cuando se mueve al prota, se mueve automáticamente al malo
-        let timer = setTimeout(moverMalo(this.posicion_prota),1000);
-    }
 }
 
-//Función que mueve al malo
-function moverMalo(pos_prota) {
-    console.log(pos_prota)
-    let celdas = Array.from(document.getElementsByTagName("td"));
-    celdas.forEach(e =>{
-        if(e.id == "malo"){
-            let posicion_actual_malo = celdas.indexOf(e);
+function moverMalo(celdas, pos_prota) {
+    //Cambio la colección html a array
+    celdas = Array.from(celdas)
+
+    //Creo la matriz donde voy a almacenar las celdas
+    let matriz = [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        []
+    ];
+
+    //Creo dos variables que usaré para almacenar las celdas
+    let contador = 0;
+    let indice = 0;
+    celdas.forEach(element => {
+        indice++;
+        matriz[contador].push(element);
+        if (indice % 8 == 0) {
+            contador++;
         }
-    })
-    
+        //Guardo las coordenadas del protagonista
+        if(element.id =="prota"){
+            let filaProta = contador;
+            let columnaProta = matriz[filaProta].indexOf(element);
+        }
+    });
+
+    //Recorro la matriz para cambiar la posición del malo en función de las coordenadas del prota
+    for (let i = 0; i < matriz.length; i++) {
+        for (let j = 0; j < matriz[i].length; j++) {
+            if (matriz[i][j].id == "malo") {
+                matriz[i][j].id = "";
+                if(i < fila){
+                    matriz[i-1][j].id = "malo";
+                }
+            }
+        }
+    }
+
+    // for (const e of matriz) {
+    //     for (const i of e) {
+    //         if(i.id == "prota"){
+
+    //         }
+    //         if(i.id == "malo"){
+    //             i.id == "";
+    //         }
+    //     }
+    // }
+
+
+
+
+
+    // //Recorro las celdas de la tabla para encontrar la del malo
+    // celdas.forEach(e => {
+    //     if (e.id == "malo") {
+    //         e.id = "";
+    //         // if (pos_prota < celdas.indexOf(e)) {
+    //         //     celdas[celdas.indexOf(e) - 1].id = "malo";
+    //         // }
+    //         if ((pos_prota == celdas.indexOf(e) - 8) || (pos_prota == celdas.indexOf(e) - 16) || (pos_prota == celdas.indexOf(e) - 24) || (pos_prota == celdas.indexOf(e) - 32) || (pos_prota == celdas.indexOf(e) - 40) || (pos_prota == celdas.indexOf(e) - 48) || (pos_prota == celdas.indexOf(e) - 56)) {
+    //             celdas[celdas.indexOf(e) - 8].id = "malo";
+    //         }
+    //     }
+    // });
 }
