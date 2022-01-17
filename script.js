0a470e3a5b2a6ebfb3f749a527048510548d7cf0
Josemi74 <jld0002@alu.medac.es>
2022-01-17 18:44:33 +0100

    Fichero script (Se mueve el prota y coge los examenes)

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
 window.onload = () =>{
-    // Posición inicial prota
-    let posicion_prota = 0;
-
-    // Genero la posición de los examenes y los coloco en el tablero
-    let posicion_examenes = parseInt(Math.random()*62+1);
-    let examenes = document.querySelectorAll("td")[posicion_examenes];
-    examenes.id = "examenes";
+    // Selecciono todas las celdas de la tabla
+    let celdas = document.querySelectorAll("td");
     
-    // Selecciono la celda en la que se encuentra el prota
-    let prota = document.querySelectorAll("td")[posicion_prota];
+    // Coloco al protagonista y guardo su posición en una variable
+    let posicion_prota = 0;
+    celdas[posicion_prota].id = "prota";
 
-    // Asigno a la celda en la que se encuentra el prota el id "prota"
-    prota.id = "prota";
-
-    // Selecciono el formulario y los botones que hay en su interior
-    let form = document.querySelector("form");
-    let arriba = form.arriba;
-    let izquierda = form.izquierda;
-    let derecha = form.derecha;
-    let abajo = form.abajo;
-
-    // Deshabilito los botones de arriba e izquierda
-    arriba.disabled = true;
-    izquierda.disabled = true;
+    // Genero la posición de los examenes, guardo dicha posición en una variable y coloco los examenes en el tablero
+    let posicion_examenes = parseInt(Math.random()*62+1);
+    celdas[posicion_examenes].id = "examenes";
 
     // Creo una variable inicializada a "false" para almacenar los examenes (false: no tiene los examenes  --  True: tiene los examenes)
     let examanes_inventario = false;
@@ -30,76 +16,28 @@ window.onload = () =>{
     // Selecciono el "inventario"
     let inventario = document.getElementById("inventario");
 
-    // Mover hacia abajo
-    abajo.onclick = () => {
-        if (arriba.disabled) {
-            arriba.disabled = false;
+    // Recorro todas las celdas de la tabla
+    celdas.forEach((i)=>{
+        // Añado el evento "onclick" a cada una de las celdas
+        i.onclick = (e)=>{
+            // Compruebo si el movimiento que quiere hacer el usuario es correcto
+            if (posicion_prota+1==Array.from(celdas).indexOf(e.target) || posicion_prota-1==Array.from(celdas).indexOf(e.target) || posicion_prota+8==Array.from(celdas).indexOf(e.target) || posicion_prota-8==Array.from(celdas).indexOf(e.target)) {
+                celdas[posicion_prota].id = "";
+                posicion_prota = Array.from(celdas).indexOf(e.target);
+                e.target.id = "prota";
+                // Compruebo si el protagonista ha conseguido los examenes
+                if (posicion_prota == posicion_examenes) {
+                    examanes_inventario = true;
+                    inventario.textContent = "Examenes";
+                }
+            }
+            else if (posicion_prota==Array.from(celdas).indexOf(e.target)) {
+                alert("El protagonista no se puede quedar en la misma posición. Intente moverlo a otra posición diferente a la que se encuentra.");
+            }
+            else{
+                alert("Ha esa posición no se puede mover. Intentelo de nuevo con otra posición.");
+            }
         }
-        prota.id = "";
-        posicion_prota += 8;
-        prota = document.querySelectorAll("td")[posicion_prota];
-        prota.id = "prota";
-        if (posicion_prota>=56 && posicion_prota<=63) {
-            abajo.disabled = true;
-        }
-        if (posicion_prota == posicion_examenes) {
-            examanes_inventario = true;
-            inventario.textContent = "Examenes";
-        }
-    }
+    });
 
-    // Mover hacia arriba
-    arriba.onclick = () => {
-        if (abajo.disabled) {
-            abajo.disabled = false;
-        }
-        prota.id = "";
-        posicion_prota -= 8;
-        prota = document.querySelectorAll("td")[posicion_prota];
-        prota.id = "prota";
-        if (posicion_prota>=0 && posicion_prota<=7) {
-            arriba.disabled = true;
-        }
-        if (posicion_prota == posicion_examenes) {
-            examanes_inventario = true;
-            inventario.textContent = "Examenes";
-        }
-    }
-
-    // Mover hacia la izquierda
-    izquierda.onclick = () => {
-        if (derecha.disabled) {
-            derecha.disabled = false;
-        }
-        prota.id = "";
-        posicion_prota -= 1;
-        prota = document.querySelectorAll("td")[posicion_prota];
-        prota.id = "prota";
-        if (posicion_prota%8==0 || posicion_prota==0) {
-            izquierda.disabled = true;
-        }
-        if (posicion_prota == posicion_examenes) {
-            examanes_inventario = true;
-            inventario.textContent = "Examenes";
-        }
-    }
-
-    // Mover hacia la derecha
-    derecha.onclick = () => {
-        if (izquierda.disabled) {
-            izquierda.disabled = false;
-        }
-        const fin_derecha = [7,15,23,31,39,47,55,63];
-        prota.id = "";
-        posicion_prota += 1;
-        prota = document.querySelectorAll("td")[posicion_prota];
-        prota.id = "prota";
-        if (fin_derecha.includes(posicion_prota)) {
-            derecha.disabled = true;
-        }
-        if (posicion_prota == posicion_examenes) {
-            examanes_inventario = true;
-            inventario.textContent = "Examenes";
-        }
-    }
 }
